0b4404988b55375d3a46552057e5a1a278e498fe
ISN-Technologies <ibra.isn@gmail.com>
2023-02-17 10:37:31 +0300

    2023 Site Locked

--- pages/index[0riginal].vue
+++ pages/index[0riginal].vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="container">
-    <h1 style="margin-top: 30px; margin-bottom: 20px; color: #ff0000; text-align: center">Oops!</h1>
-    <h2 style="margin-top: 20px; margin-bottom: 20px; color: #ff0000; text-align: center">Your Website is Locked</h2>
-    <p style="margin-top: 20px; margin-bottom: 20px;  text-align: center">Please contact your website <a href="mailto:info@isntechnologies.co.tz"> administartor </a> to unlock your website.</p>
+  <div>
+    <NavThree/>
+    <SliderTwo/>
+<!--    <NanoArea/>-->
+    <SolutionArea/>
+    <VideoArea/>
+    <BuildingArea/>
+    <BrandArea/>
+<!--    <ClientArea/>-->
+<!--    <FunFactArea/>-->
+<!--    <ProjectArea/>-->
+    <FaqBrandArea/>
+    <FaqArea/>
+    <FutureArea/>
+    <FooterTwo/>
   </div>
 </template>
 
