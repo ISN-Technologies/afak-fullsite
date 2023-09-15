a2edec24b5be5cfa7e8d197e1661653de9e9171c
Ibrahim Nkya <ibra.isn@gmail.com>
2023-09-15 15:19:50 +0300

    Site Upload

---
+++ pages/index[construction].vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1 style="margin-top: 30px; margin-bottom: 20px; color: #77c72d; text-align: center">Sorry!</h1>
+    <h2 style="margin-top: 20px; margin-bottom: 20px; color: #c7992d; text-align: center"> &#x1F6E0; Our website is currently under
+      maintenance </h2>
+    <p style="margin-top: 20px; margin-bottom: 20px;  text-align: center">
+      For enquiries you can reach to us via
+      <a
+      href="mailto:info@afaktanzania.co.tz"
+      style="text-decoration: none; color: #0056b3"
+      ><i class="fa fa-envelope-o" aria-hidden="true"></i>
+      email</a> or chat with us on
+      <a
+        href="wa.link/yrysqm"
+        style="text-decoration: none; color: #1FAA09"
+      ><i class="fa fa-whatsapp" aria-hidden="true"></i>
+        whatsapp.</a></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Index"
+}
+</script>
